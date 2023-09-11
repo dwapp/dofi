91f69ba0801db8b40f59d0748b38fdcedfca342e
rewine <luhongxu@deepin.org>
2023-09-11 15:10:27 +0800

    rename to dofi

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
-    const QUrl url(u"qrc:/qt/qml/DLauncher/Main.qml"_qs);
+    const QUrl url(u"qrc:/qt/qml/Dofi/Main.qml"_qs);
 #else
-    const QUrl url(u"qrc:/DLauncher/Main.qml"_qs);
+    const QUrl url(u"qrc:/Dofi/Main.qml"_qs);
 #endif
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
                      &app, []() { QCoreApplication::exit(-1); },
