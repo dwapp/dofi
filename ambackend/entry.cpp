6c0ee44e2f562a9431b52fb59338cbe8c6bac52f
rewine <luhongxu@deepin.org>
2023-09-11 10:50:16 +0800

    fix am

--- ambackend/entry.cpp
+++ ambackend/entry.cpp
@@ -2,17 +2,26 @@
 #include "application.h"
 
 #include <QDBusConnection>
+#include <QDBusArgument>
 #include <QCoreApplication>
 #include <QList>
+#include <QDBusMetaType>
 
 #include <algorithm>
 #include <QDebug>
 #include <types.h>
 
 int main (int argc, char *argv[]) {
-    qRegisterMetaType<ObjectInterfaceMap>("ObjectInterfaceMap");
-    qRegisterMetaType<ObjectMap>("ObjectMap");
+    qDBusRegisterMetaType<QMap<QString, QString>>();
+
     qRegisterMetaType<PropMap>("PropMap");
+    qDBusRegisterMetaType<PropMap>();
+
+    qRegisterMetaType<ObjectMap>("ObjectMap");
+    qDBusRegisterMetaType<ObjectMap>();
+
+    qRegisterMetaType<ObjectInterfaceMap>("ObjectInterfaceMap");
+    qDBusRegisterMetaType<ObjectInterfaceMap>();
 
     QCoreApplication app(argc, argv);
     ApplicationManager *manager = new ApplicationManager;
@@ -26,7 +35,7 @@ int main (int argc, char *argv[]) {
 
     qDebug() << "begin:" << list.size();
     for (Application* app : list) {
-        qDebug() << "name:" << " " << app->displayName() << " id: " << app->iD();
+        qDebug() << "name:" << " " << app->displayName() << " id: " << app->iD() << "icons: " << app->icons();
         if (app->iD() == "xterm") {
             app->Launch();
         }
--- ambackend/types.h
+++ ambackend/types.h
@@ -6,15 +6,12 @@
 #include <QDBusConnection>
 #include <QDBusObjectPath>
 
-using IconMap = QMap<QString, QMap<uint, QMap<QString, QDBusUnixFileDescriptor>>>;
+//using IconMap = QMap<QString, QMap<uint, QMap<QString, QDBusUnixFileDescriptor>>>;
 using ObjectInterfaceMap = QMap<QString, QVariantMap>;
 using ObjectMap = QMap<QDBusObjectPath, ObjectInterfaceMap>;
 using PropMap = QMap<QString, QMap<QString, QString>>;
 
 Q_DECLARE_METATYPE(ObjectInterfaceMap)
-
 Q_DECLARE_METATYPE(ObjectMap)
-
-
 Q_DECLARE_METATYPE(PropMap)
 
