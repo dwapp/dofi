fb78dd068442e3715c766549103bf2502982e107
rewine <luhongxu@deepin.org>
2023-09-11 15:05:10 +0800

    qtm0001

--- main.cpp
+++ main.cpp
@@ -13,7 +13,11 @@ int main(int argc, char *argv[])
     parser.addVersionOption();
 
     QQmlApplicationEngine engine;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
+    const QUrl url(u"qrc:/qt/qml/DLauncher/Main.qml"_qs);
+#else
     const QUrl url(u"qrc:/DLauncher/Main.qml"_qs);
+#endif
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
                      &app, []() { QCoreApplication::exit(-1); },
     Qt::QueuedConnection);
